cad02548636b54fd1135033d5237640e115b2446
saehaana <61268356+saehaana@users.noreply.github.com>
2022-08-04 13:07:27 -0400

    changed order of activities, added temporary functionality to login button to access main activity screen, fixed text visibility warnings

---
+++ app/src/main/java/com/example/cubeinone/LoginScreen.kt
@@ -0,0 +1,27 @@
+package com.example.cubeinone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginScreen : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+
+        //store id in value
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        //login button takes us to MainActivity page
+        buttonLogin?.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+}
