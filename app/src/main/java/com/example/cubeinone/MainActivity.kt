0493c15288e6bbf0bafbbd1125f4800b9b6c338a
saehaana <61268356+saehaana@users.noreply.github.com>
2022-08-06 22:19:09 -0400

    added function timer() to textview timer, to be used as stopwatch

--- app/src/main/java/com/example/cubeinone/MainActivity.kt
+++ app/src/main/java/com/example/cubeinone/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.cubeinone
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         val layout = findViewById<ConstraintLayout>(R.id.layoutMainActivity)
 
-        layout.setOnTouchListener{v: View, m: MotionEvent ->
+        layout.setOnTouchListener{ v: View, m: MotionEvent ->
             handleTouch(m)
             true
         }
@@ -27,8 +30,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleTouch(m: MotionEvent){
         when(m.actionMasked){
-            MotionEvent.ACTION_DOWN -> Log.i("TouchEvents","Action Down")
-            MotionEvent.ACTION_UP -> Log.i("TouchEvents","Action Up")
+            MotionEvent.ACTION_UP -> timer() //detects when touch on layout is released
         }
     }
+
+    private fun timer() {
+        val countTime: TextView = findViewById(R.id.timer)
+        object: CountDownTimer(Long.MAX_VALUE,100){
+            override fun onTick(millisUntilFinished: Long) {
+                countTime.text = counter.toString()
+                counter++
+            }
+            @SuppressLint("SetTextI18n")
+            override fun onFinish() {
+                countTime.text = "Finished"
+            }
+        }.start()
+    }
+
+    
+
 }
