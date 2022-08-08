2db0a72c12b166784a13fc91ae6196e12539ab27
saehaana <61268356+saehaana@users.noreply.github.com>
2022-08-08 19:22:07 -0400

    added captureVideo function: when finger is released off screen recording session will start

--- app/src/main/java/com/example/cubeinone/MainActivity.kt
+++ app/src/main/java/com/example/cubeinone/MainActivity.kt
@@ -2,39 +2,54 @@ package com.example.cubeinone
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Intent
+import android.content.ContentValues
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.provider.MediaStore
-import android.provider.SyncStateContract
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
-import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.os.HandlerCompat.postDelayed
+import androidx.core.content.PermissionChecker
+import com.example.cubeinone.MainActivity.Constants.FILE_FORMAT
 import com.example.cubeinone.MainActivity.Constants.REQUEST_CODE_PERMISSIONS
 import com.example.cubeinone.MainActivity.Constants.REQUIRED_PERMISSIONS
+import com.example.cubeinone.MainActivity.Constants.TAG
 import com.example.cubeinone.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.security.Permission
 import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity(){
-    var counter = 0
-    private var seconds = 0
-    private var running : Boolean = true
-    private var wasRunning : Boolean = true
-
     /* one binding object that has references for all views with id */
     private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
 
+    //Constant values used for permissions
+    object Constants{
+        const val TAG = "cameraX"
+        const val FILE_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
+        const val REQUEST_CODE_PERMISSIONS = 10
+        val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,32 +73,38 @@ class MainActivity : AppCompatActivity(){
         */
         setContentView(binding.root)
 
+        //Checks for user permissions status, will request permission if not yet granted
         if(allPermissionsGranted()){
             Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show()
         }else{
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,REQUEST_CODE_PERMISSIONS)
         }
 
-    }
-    object Constants{
-        const val TAG = "cameraX"
-        const val FILE_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
-        const val REQUEST_CODE_PERMISSIONS = 10
-        val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        binding.viewFinder.setOnTouchListener{v: View, m: MotionEvent ->
+            handleTouch(m)
+            true
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
     }
 
+    //checks if a particular permission has been granted
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
             ContextCompat.checkSelfPermission(baseContext,it) == PackageManager.PERMISSION_GRANTED
     }
 
+    /*
+    *
+    * */
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
         IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) { //checks if request code is correct
+            if (allPermissionsGranted()) { //starts the camera if permissions are granted
                 startCamera()
-            } else {
+            } else { //notifies user permissions weren't given
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
                     Toast.LENGTH_SHORT).show()
@@ -92,8 +113,111 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+    //lets user preview what they're capturing
     private fun startCamera() {
-        TODO("Not yet implemented")
+        //binds lifecycle of cameras to lifecycle owner
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            //initialize preview object
+            val preview = Preview.Builder().build().also{
+                it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+            }
+
+            //creates video capture use case
+            val recorder = Recorder.Builder()
+                .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+                .build()
+            videoCapture = VideoCapture.withOutput(recorder)
+
+            //back camera is selected by default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            //bind use cases to camera
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture)
+            }catch(exc: Exception){
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun captureVideo(){
+        //checks if VideoCapture use case has been created
+        val videoCapture = this.videoCapture ?: return
+
+        //disables UI until request action completed by CameraX, re-enabled in VideoRecordListener
+        binding.layoutMainActivity.isEnabled = false
+
+        val curRecording = recording
+        //if recording in progress, stop and release current recording
+        if(curRecording != null){
+            curRecording.stop()
+            recording = null
+            return
+        }
+
+        //create and start new recording
+        val name = SimpleDateFormat(FILE_FORMAT, Locale.US).format(System.currentTimeMillis())
+
+        val contentValues = ContentValues().apply{
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+            }
+        }
+
+        val mediaStoreOutputOptions = MediaStoreOutputOptions.Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI).setContentValues(contentValues).build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            return
+        }
+        recording = videoCapture.output.prepareRecording(this, mediaStoreOutputOptions).withAudioEnabled().apply{
+            if(PermissionChecker.checkSelfPermission(this@MainActivity,Manifest.permission.RECORD_AUDIO) == PermissionChecker.PERMISSION_GRANTED){
+                withAudioEnabled()
+            }
+        }
+        .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+            when(recordEvent) {
+                is VideoRecordEvent.Finalize -> {
+                    if (!recordEvent.hasError()) {
+                        val msg = "Video capture succeeded: " +
+                                "${recordEvent.outputResults.outputUri}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
+                            .show()
+                        Log.d(TAG, msg)
+                    } else {
+                        recording?.close()
+                        recording = null
+                        Log.e(TAG, "Video capture ends with error: " +
+                                "${recordEvent.error}")
+                    }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun handleTouch(m: MotionEvent){
+        when(m.actionMasked){
+            MotionEvent.ACTION_UP -> captureVideo() //detects when touch is released
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
     /*
